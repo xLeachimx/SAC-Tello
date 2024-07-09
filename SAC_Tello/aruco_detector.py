c13389e3e9e40364ed455b4ff9a4e92a56fb9455
DrHuelsman <mhuelsman@anselm.edu>
2024-07-09 14:27:10 -0400

    Distance update for aruco (thanks to A. Albina for sourcing a solution)

--- SAC_Tello/aruco_detector.py
+++ SAC_Tello/aruco_detector.py
@@ -57,12 +57,39 @@ class ArucoDetector:
         if len(corners) == 0:
             return detected_markers
         for location, num in zip(corners, ids):
-            # pose = aruco.estimatePoseSingleMarkers(location, self._MARKER_SIZE_CM, self._CAMERA_PARAMETERS,
-            #                                        self._DISTORTION_PARAMETERS)
-            # translation = pose[1]
-            # distance = int(np.linalg.norm(translation))
-            detected_markers.append((num, ArucoDetector.__convert_corners_to_rect(location), -1))
+            nada, R, t = ArucoDetector.my_estimatePoseSingleMarkers(location, self._MARKER_SIZE_CM,
+                                                     self._CAMERA_PARAMETERS,
+                                                     self._DISTORTION_PARAMETERS)
+            distance = int(R[0][0][2])
+            detected_markers.append((num, ArucoDetector.__convert_corners_to_rect(location), distance))
         return detected_markers
+
+    @staticmethod
+    def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
+        """
+        This will estimate the rvec and tvec for each of the marker corners detected by:
+           corners, ids, rejectedImgPoints = detector.detectMarkers(image)
+        corners - is an array of detected corners for each detected marker in the image
+        marker_size - is the size of the detected markers
+        mtx - is the camera matrix
+        distortion - is the camera distortion matrix
+        RETURN list of rvecs, tvecs, and trash (so that it corresponds to the old estimatePoseSingleMarkers())
+        """
+        marker_points = np.array([[-marker_size / 2, marker_size / 2, 0],
+                                  [marker_size / 2, marker_size / 2, 0],
+                                  [marker_size / 2, -marker_size / 2, 0],
+                                  [-marker_size / 2, -marker_size / 2, 0]], dtype=np.float32)
+        trash = []
+        rvecs = []
+        tvecs = []
+
+        for c in corners:
+            nada, R, t = cv.solvePnP(marker_points, c, mtx, distortion, False, cv.SOLVEPNP_IPPE_SQUARE)
+            rvecs.append(R)
+            tvecs.append(t)
+            trash.append(nada)
+        # return rvecs, tvecs, trash
+        return np.array([rvecs]), np.array([tvecs]), trash
 
     @staticmethod
     def __convert_corners_to_rect(location: list):
